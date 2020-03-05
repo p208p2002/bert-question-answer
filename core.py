0026a980466c33ffec3e566a8b69f8dd2d3cc2bb
Philip <p208p2002@gmail.com>
2020-03-05 07:14:24 +0000

    no message

--- core.py
+++ core.py
@@ -17,31 +17,47 @@ def convert_data_to_feature(context,question,tokenizer,doc_strike=128):
         return tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
     
     # def
-    len_limit_remain = 512 # bert input length limit
-    special_token_length = 2
+    bert_input_len_limit = 512 # bert input length limit
+    special_token_length = 3 # [CLS]A[SEP]B[SEP]
     cls_token = '[CLS]'
     sep_token = '[SEP]'
 
     #
     question = question[:100] # limit question length
-    len_limit_remain -= len(question)
+    len_limit_remain = bert_input_len_limit - len(question)
     context_ids = convert_text_to_ids(context)
 
     logger.debug(context_ids)
     logger.debug(tokenizer.decode(context_ids))
     logger.debug(len_limit_remain)
 
-    index = 0
+    next_index = 0
+    index = next_index
     window_size = len_limit_remain - special_token_length
     logger.debug("context length:%d"%(len(context)))
-    while(len(context) > window_size):        
-        logger.debug("start_index:%d"%(index))
+    token_embeddings_list = []
+    while(True):
+        next_index = index+doc_strike
+        logger.debug("start_index:%d window_size:%d next_index:%d"%(index, window_size, next_index))
+        
         input_context = context[index:index+window_size]
-        input_context = cls_token + input_context + sep_token
-        logger.debug(input_context)
+        logger.debug("input context len:%d"%(len(input_context)))
+
+        full_input = cls_token + input_context + sep_token + question + sep_token        
+        logger.debug(full_input)
+        
+        token_embeddings = convert_text_to_ids(full_input)
+        assert len(token_embeddings) <= bert_input_len_limit
+        token_embeddings_list .append(token_embeddings)
+
+        logger.debug('input token length:%d\n',len(token_embeddings))
+
+        # already process the end of input context
+        if(len(input_context) < window_size):
+            break
 
         #
-        index += window_size
-        context = context[index:]
-
+        index = next_index
+    #
+    return token_embeddings_list
 
