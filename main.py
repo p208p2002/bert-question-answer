abf188e31b0f60b157107fd9266a4c579c3a14ef
Philip <p208p2002@gmail.com>
2020-03-05 01:58:00 +0000

    no message

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from core import use_model
+import os
 if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+    model_setting = {
+        "model_name":"bert", 
+        "config_file_path":"trained_model/config.json", 
+        "model_file_path":"trained_model/pytorch_model.bin", 
+        "vocab_file_path":"trained_model/vocab.txt"
+    }    
+    model,tokenizer = use_model(**model_setting)
+
     context = "王大明在文德國小擔任校長"
     question = "誰擔任校長"
