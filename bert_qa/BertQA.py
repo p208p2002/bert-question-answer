1677eff747383243e53a10be768410896bc126f9
Philip <p208p2002@gmail.com>
2020-03-09 03:27:44 +0000

    優化速度

--- bert_qa/BertQA.py
+++ bert_qa/BertQA.py
@@ -1,6 +1,7 @@
 from .core import use_model,convert_single_data_to_feature,make_torch_data_loader,make_torch_dataset,to_list, \
     _get_best_indexes,_check_has_skip_token,_check_segment_type_is_b
 import logging
+from tqdm import tqdm
 logger = logging.getLogger(__name__)
 class BertQA():
     def __init__(self,model,tokenizer,device,logging_level=logging.WARNING):
@@ -25,6 +26,7 @@ class BertQA():
         qc_dataset = make_torch_dataset(token_embeddings_list, segment_embeddings_lsit, attention_embeddings_list)
         qc_data_loader = make_torch_data_loader(qc_dataset,batch_size=batch_size)
         answer_results = []
+        pbar = tqdm(total=len(qc_data_loader))
         for index,batch in enumerate(qc_data_loader):
             batch = [x.to(device) for x in batch]
             start_scores, end_scores = model(input_ids=batch[0],token_type_ids=batch[1],attention_mask=batch[2])
@@ -55,18 +57,27 @@ class BertQA():
                         answer_results.append(answer_result)
                         logger.debug("batch_index:%d"%(index))
                         # logger.debug("start_index:%d(%3.5f) end_index:%d(%3.5f) answer:%s"%(answer_result[0],answer_result[1],answer_result[2],answer_result[3],answer_result[4]))
+            pbar.update()
                 
         answer_records = []
         answer_results = sorted(answer_results,key=lambda answer_result:answer_result[1]+answer_result[3],reverse=True)
+
+        pbar = tqdm(total=n_best_size)
+        n_best_answer_results = []
         for answer_result in answer_results[:]:
             answer_tag = answer_result[4]
             if(answer_tag not in answer_records):
                 answer_records.append(answer_tag)
+                n_best_answer_results.append(answer_result)
             else:
-                answer_results.remove(answer_result)
+                # answer_results.remove(answer_result)
                 continue
             logger.debug("score:%3.5f start_index:%d(%3.5f) end_index:%d(%3.5f) answer:%s"\
                 %(answer_result[1]+answer_result[3],answer_result[0],answer_result[1],answer_result[2],answer_result[3],answer_result[4]))
-        return answer_results[:n_best_size]
+
+            pbar.update()            
+            if(len(n_best_answer_results) >= n_best_size):
+                return n_best_answer_results
+        return n_best_answer_results
         
         
